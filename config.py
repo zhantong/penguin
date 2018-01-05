5299b8832e628c72ada572b5e6c4e63851579fa8
Tong ZHAN <zhantong1994@163.com>
2018-01-05 13:54:05 +0800

    添加Bootstrap支持 使用flask-bootstrap

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    BOOTSTRAP_SERVE_LOCAL = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
