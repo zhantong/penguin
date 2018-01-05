2063d077d73f0263d264e3b3099ca1f6c93405eb
Tong ZHAN <zhantong1994@163.com>
2018-01-05 14:56:30 +0800

    在更新文章时将Markdown渲染成HTML

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from datetime import datetime
+import markdown2
 
 
 class Post(db.Model):
@@ -8,5 +9,13 @@ class Post(db.Model):
     title = db.Column(db.String(200))
     slug = db.Column(db.String(200))
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        target.body_html = markdown2.markdown(value)
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
