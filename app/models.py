466cd8b980f782c8340f2f5d2aa673ea9b925b84
Tong ZHAN <zhantong1994@163.com>
2018-01-05 15:10:47 +0800

    在更新文章时由HTML生成文章摘要 目前是文章的前200个字符

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from . import db
 from datetime import datetime
 import markdown2
+import re
+
+RE_HTML_TAGS = re.compile(r'<[^<]+?>')
 
 
 class Post(db.Model):
@@ -10,12 +13,14 @@ class Post(db.Model):
     slug = db.Column(db.String(200))
     body = db.Column(db.Text)
     body_html = db.Column(db.Text)
+    body_abstract = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     @staticmethod
     def on_changed_body(target, value, oldvalue, initiator):
         target.body_html = markdown2.markdown(value)
+        target.body_abstract = RE_HTML_TAGS.sub('', target.body_html)[:200] + '...'
 
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
