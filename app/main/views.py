48a193d0fdbf13cd7c5d204711dae17c60ffea9d
Tong ZHAN <zhantong1994@163.com>
2018-01-05 15:48:12 +0800

    实现查看文章内容

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,9 @@ from ..models import Post
 def index():
     posts = Post.query.order_by(Post.timestamp.desc())
     return render_template('index.html', posts=posts)
+
+
+@main.route('/archives/<slug>.html')
+def show_post(slug):
+    post = Post.query.filter_by(slug=slug).first_or_404()
+    return render_template('post.html', post=post)
