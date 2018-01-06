fb7242a6c27cd8f9a2f76ee3cb25960a62106b68
Tong ZHAN <zhantong1994@163.com>
2018-01-06 15:45:06 +0800

    增加后台框架

--- app/auth/views.py
+++ app/auth/views.py
@@ -14,7 +14,7 @@ def login():
             login_user(user, form.remember_me.data)
             next = request.args.get('next')
             if next is None or not next.startswith('/'):
-                next = url_for('main.index')
+                next = url_for('admin.index')
             return redirect(next)
         flash('用户名或密码错误')
     return render_template('auth/login.html', form=form)
