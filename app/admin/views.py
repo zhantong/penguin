8241f9a91343180b51c223ec430f8a03ec6edf0a
Tong ZHAN <zhantong1994@163.com>
2018-01-06 16:46:18 +0800

    增加撰写文章界面，集成Markdown编辑器

--- app/admin/views.py
+++ app/admin/views.py
@@ -6,3 +6,8 @@ from . import admin
 @admin.route('/')
 def index():
     return render_template('admin/index.html')
+
+
+@admin.route('/write-post')
+def write_post():
+    return render_template('admin/write-post.html')
