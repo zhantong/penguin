484c2a7d390ca3051862de653f84f0ec9cc5f6a1
Tong ZHAN <zhantong1994@163.com>
2018-01-06 21:02:39 +0800

    为管理员的所有页面添加登录验证

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,12 @@
 from flask import render_template
-
+from flask_login import login_required
 from . import admin
+
+
+@admin.before_request
+@login_required
+def before_request():
+    pass
 
 
 @admin.route('/')
@@ -11,3 +17,8 @@ def index():
 @admin.route('/write-post')
 def write_post():
     return render_template('admin/write-post.html')
+
+
+@admin.route('/manage-posts')
+def manage_posts():
+    return render_template('admin/manage-posts.html')
