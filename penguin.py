1dd5fe3f3b984b3563587003236953f6055b6e05
Tong ZHAN <zhantong1994@163.com>
2018-01-09 09:23:07 +0800

    增加初始化命令

--- penguin.py
+++ penguin.py
@@ -1,4 +1,13 @@
 import os
-from app import create_app
+from app import create_app, db
+from app.models import PostType, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+
+@app.cli.command()
+def deploy():
+    db.create_all()
+
+    Role.insert_roles()
+    PostType.insert_post_types()
