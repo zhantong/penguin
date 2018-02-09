dd0ae2a318754762a82f35c29cb8594624aa8fc3
Tong ZHAN <zhantong1994@163.com>
2018-02-09 20:07:57 +0800

    支持自定义错误页面

--- app/main/views.py
+++ app/main/views.py
@@ -15,3 +15,13 @@ def index():
                        right_widgets=right_widgets, scripts=scripts)
     return render_template('index.html', **request.args.to_dict(), **context, styles=styles, contents=contents,
                            left_widgets=left_widgets, right_widgets=right_widgets, scripts=scripts)
+
+
+@main.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@main.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
