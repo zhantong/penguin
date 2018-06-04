8320ba8d58db5eabc39bcb968383b8b7120f90fa
Tong ZHAN <zhantong1994@163.com>
2018-06-05 01:06:52 +0800

    支持导出typecho中数据到json文件中

---
+++ migrations/typecho/Dump.py
@@ -0,0 +1,156 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import User, Content, Comment, Meta, Relationship
+import json
+import phpserialize
+
+
+class Dump:
+    def __init__(self, db_url):
+        engine = create_engine(db_url)
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+        self.data = {}
+
+    @staticmethod
+    def process_comments(comments, parent=0):
+        result = []
+        for comment in comments:
+            if comment.parent == parent:
+                comments.remove(comment)
+                co = {
+                    'body': comment.text,
+                    'timestamp': comment.created,
+                    'ip': comment.ip,
+                    'agent': comment.agent
+                }
+                if comment.authorId != 0:
+                    co['author'] = comment.author
+                else:
+                    co['author'] = {
+                        'name': comment.author,
+                        'email': comment.mail,
+                        'member_since': comment.created
+                    }
+                co['children'] = Dump.process_comments(comments, comment.coid)
+                result.append(co)
+        return result
+
+    @staticmethod
+    def process_attachements(attachments):
+        result = []
+        for attachment in attachments:
+            meta = {}
+            for key, value in phpserialize.loads(bytes(attachment.text, 'utf-8')).items():
+                if type(key) is bytes:
+                    key = key.decode('utf-8')
+                if type(value) is bytes:
+                    value = value.decode('utf-8')
+                meta[key] = value
+            result.append({
+                'original_filename': attachment.title,
+                'file_path': meta['path'],
+                'mime': meta['mime'],
+                'timestamp': attachment.created
+            })
+        return result
+
+    @staticmethod
+    def process_categories(categories):
+        return [category.name for category in categories]
+
+    @staticmethod
+    def process_tags(tags):
+        return [tag.name for tag in tags]
+
+    def dump_admin(self):
+        self.data['admin'] = []
+        for administrator in self.session.query(User).filter_by(group='administrator'):
+            admin = {
+                'username': administrator.name,
+                'name': administrator.screenName,
+                'email': administrator.mail,
+                'member_since': administrator.created
+            }
+            self.data['admin'].append(admin)
+
+    def dump_category(self):
+        self.data['category'] = []
+        for category in self.session.query(Meta).filter_by(type='category'):
+            c = {
+                'name': category.name,
+                'description': category.description
+            }
+            self.data['category'].append(c)
+
+    def dump_tag(self):
+        self.data['tag'] = []
+        for tag in self.session.query(Meta).filter_by(type='tag'):
+            t = {
+                'name': tag.name,
+                'description': tag.description
+            }
+            self.data['tag'].append(t)
+
+    def dump_article(self):
+        self.data['article'] = []
+        for post in self.session.query(Content).filter_by(type='post'):
+            article = {
+                'title': post.title,
+                'slug': post.slug,
+                'body': post.text.replace('<!--markdown-->', ''),
+                'timestamp': post.created,
+                'author': self.session.query(User).filter_by(uid=post.authorId).one().name
+            }
+
+            comments = self.session.query(Comment).filter_by(cid=post.cid).order_by(Comment.created).all()
+            comments = Dump.process_comments(comments)
+            article['comments'] = comments
+
+            attachments = self.session.query(Content).filter_by(type='attachment', parent=post.cid).all()
+            attachments = Dump.process_attachements(attachments)
+            article['attachments'] = attachments
+
+            categories = self.session.query(Meta).filter(Relationship.cid == post.cid).filter(
+                Relationship.mid == Meta.mid).filter_by(type='category').all()
+            categories = Dump.process_categories(categories)
+            article['categories'] = categories
+
+            tags = self.session.query(Meta).filter(Relationship.cid == post.cid).filter(
+                Relationship.mid == Meta.mid).filter_by(type='tag').all()
+            tags = Dump.process_tags(tags)
+            article['tags'] = tags
+
+            self.data['article'].append(article)
+
+    def dump_page(self):
+        self.data['page'] = []
+        for page in self.session.query(Content).filter_by(type='page'):
+            p = {
+                'title': page.title,
+                'slug': page.slug,
+                'body': page.text.replace('<!--markdown-->', ''),
+                'timestamp': page.created,
+                'author': self.session.query(User).filter_by(uid=page.authorId).one().name
+            }
+
+            comments = self.session.query(Comment).filter_by(cid=page.cid).order_by(Comment.created).all()
+            comments = Dump.process_comments(comments)
+            p['comments'] = comments
+
+            attachments = self.session.query(Content).filter_by(type='attachment', parent=page.cid).all()
+            attachments = Dump.process_attachements(attachments)
+            p['attachments'] = attachments
+
+            self.data['page'].append(p)
+
+
+if __name__ == '__main__':
+    dump = Dump("mysql+pymysql://root:123456@localhost/typecho?charset=utf8")
+    dump.dump_admin()
+    dump.dump_category()
+    dump.dump_tag()
+    dump.dump_article()
+    dump.dump_page()
+    with open('dump.json', 'w') as f:
+        f.write(json.dumps(dump.data))
---
+++ migrations/typecho/models.py
@@ -0,0 +1,84 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Text
+
+Base = declarative_base()
+
+
+class Comment(Base):
+    __tablename__ = 'typecho_comments'
+    coid = Column(Integer, primary_key=True)
+    cid = Column(Integer, index=True)
+    created = Column(Integer, index=True)
+    author = Column(String(200))
+    authorId = Column(Integer)
+    ownerId = Column(Integer)
+    mail = Column(String(200))
+    url = Column(String(200))
+    ip = Column(String(64))
+    agent = Column(String(200))
+    text = Column(Text)
+    type = Column(String(16))
+    status = Column(String(16))
+    parent = Column(Integer)
+
+
+class Content(Base):
+    __tablename__ = 'typecho_contents'
+    cid = Column(Integer, primary_key=True)
+    title = Column(String(200))
+    slug = Column(String(200), index=True)
+    created = Column(Integer, index=True)
+    modified = Column(Integer, index=True)
+    text = Column(Text)
+    order = Column(Integer)
+    authorId = Column(Integer)
+    template = Column(String(32))
+    type = Column(String(16))
+    status = Column(String(16))
+    password = Column(String(32))
+    commentsNum = Column(Integer)
+    allowComment = Column(String(1))
+    allowPing = Column(String(1))
+    allowFeed = Column(String(1))
+    parent = Column(Integer)
+    viewsNum = Column(Integer)
+
+
+class Meta(Base):
+    __tablename__ = 'typecho_metas'
+    mid = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    slug = Column(String(200))
+    type = Column(String(32))
+    description = Column(String(200))
+    count = Column(Integer)
+    order = Column(Integer)
+    parent = Column(Integer)
+
+
+class Option(Base):
+    __tablename__ = 'typecho_options'
+    name = Column(String(32), primary_key=True)
+    user = Column(Integer, primary_key=True)
+    value = Column(Text)
+
+
+class Relationship(Base):
+    __tablename__ = 'typecho_relationships'
+    cid = Column(Integer, primary_key=True)
+    mid = Column(Integer, primary_key=True)
+
+
+class User(Base):
+    __tablename__ = 'typecho_users'
+    uid = Column(Integer, primary_key=True)
+    name = Column(String(32))
+    password = Column(String(64))
+    mail = Column(String(200))
+    url = Column(String(200))
+    screenName = Column(String(32))
+    created = Column(Integer)
+    activated = Column(Integer)
+    logged = Column(Integer)
+    group = Column(String(16))
+    authCode = Column(String(64))
