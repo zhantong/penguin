78f0651590c1382631b44285a7fae9a04fd02fba
Tong ZHAN <zhantong1994@163.com>
2018-06-07 20:40:40 +0800

    增加文章目录

---
+++ app/plugins/article_contents/plugin.py
@@ -0,0 +1,22 @@
+import os
+from flask import url_for, render_template
+from ...main import main
+from ..post.models import Post
+from ...signals import navbar
+from . import signals
+
+
+@main.route('/list.html')
+def article_contents():
+    articles = Post.query_articles().order_by(Post.timestamp.desc()).all()
+    column_heads = []
+    columns = []
+    signals.article_contents_column_head.send(column_heads=column_heads)
+    signals.article_contents_column.send(columns=columns)
+    return render_template(os.path.join('article_contents', 'templates', 'article_contents.html'), articles=articles,
+                           column_heads=column_heads, columns=columns)
+
+
+@navbar.connect
+def navbar(sender, content):
+    content['items'].append(('文章目录', url_for('main.article_contents')))
---
+++ app/plugins/article_contents/signals.py
@@ -0,0 +1,4 @@
+from blinker import signal
+
+article_contents_column_head = signal('article_contents_column_head')
+article_contents_column = signal('article_contents_column')
