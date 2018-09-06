c462d731574cce9532033809756b726c253d9b98
Tong ZHAN <zhantong1994@163.com>
2018-09-06 21:40:22 +0800

    删除tag plugin中一些不再用到的代码

--- app/plugins/category/plugin.py
+++ app/plugins/category/plugin.py
@@ -24,38 +24,6 @@ category = Plugin('分类', 'category')
 category_instance = category
 
 
-@sidebar.connect
-def sidebar(sender, sidebars):
-    add_template_file(sidebars, Path(__file__), 'templates', 'sidebar.html')
-
-
-@show_list.connect_via('category')
-def show_list(sender, args):
-    page = args.get('page', 1, type=int)
-    pagination = Category.query.order_by(Category.name) \
-        .paginate(page, per_page=current_app.config['PENGUIN_POSTS_PER_PAGE'], error_out=False)
-    categories = pagination.items
-    head = ('', '名称', '别名', '文章数')
-    rows = []
-    for category in categories:
-        rows.append((category.id
-                     , Hyperlink('Hyperlink', category.name, url_for('.edit', type='category', id=category.id))
-                     , Plain('Plain', category.slug)
-                     , Hyperlink('Hyperlink', len(category.posts),
-                                 url_for('.show_list', type='post', sub_type='article', category=category.slug))))
-    table = Table('Table', head, rows)
-    args = args.to_dict()
-    if 'page' in args:
-        del args['page']
-    return {
-        **args,
-        'title': '分类',
-        'table': table,
-        'disable_search': True,
-        'pagination': Pagination('Pagination', pagination, '.show_list', args)
-    }
-
-
 @custom_article_list.connect
 @custom_list.connect
 def custom_list(sender, args, query):
@@ -84,60 +52,10 @@ def article_search_select(sender, selects):
     selects.append(select)
 
 
-@manage.connect_via('category')
-def manage(sender, form):
-    action = form.get('action', '', type=str)
-    if action == 'delete':
-        ids = form.getlist('id')
-        ids = [int(id) for id in ids]
-        if ids:
-            first_category_name = Category.query.get(ids[0]).value
-            for category in Category.query.filter(Category.id.in_(ids)):
-                db.session.delete(category)
-            db.session.commit()
-            message = '已删除分类"' + first_category_name + '"'
-            if len(ids) > 1:
-                message += '以及剩下的' + str(len(ids) - 1) + '种分类'
-            flash(message)
-
-
-@edit_article.connect
-def edit_article(sender, context, widgets, **kwargs):
-    context['all_category'] = Category.query.all()
-    context['category_ids'] = [category.id for category in context['post'].categories]
-    add_template_file(widgets, Path(__file__), 'templates', 'widget_content_category.html')
-
-
 @submit_article.connect
 def submit_article(sender, form, post):
     category_ids = form.getlist('category-id')
     post.categories = [Category.query.get(category_id) for category_id in category_ids]
-
-
-@edit.connect_via('category')
-def edit(sender, args, context, contents, **kwargs):
-    id = args.get('id', type=int)
-    if id is None:
-        category = None
-    else:
-        category = Category.query.get(id)
-    context['category'] = category
-    add_template_file(contents, Path(__file__), 'templates', 'content.html')
-
-
-@submit.connect_via('category')
-def submit(sender, args, form, **kwargs):
-    id = form.get('id', type=int)
-    if id is None:
-        category = Category()
-    else:
-        category = Category.query.get(id)
-    category.name = form['name']
-    category.slug = form['slug']
-    category.description = form['description']
-    if category.id is None:
-        db.session.add(category)
-    db.session.commit()
 
 
 @index.connect
