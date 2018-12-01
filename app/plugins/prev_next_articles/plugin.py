f499d9af389859df9d5306078a37edf150ca4c78
Tong ZHAN <zhantong1994@163.com>
2018-12-01 23:35:45 +0800

    更新prev_next_articles和toc适配新的signal模式

--- app/plugins/prev_next_articles/plugin.py
+++ app/plugins/prev_next_articles/plugin.py
@@ -5,20 +5,23 @@ from ..models import Plugin
 prev_next_articles = Plugin('上一篇/下一篇文章', 'prev_next_articles')
 prev_next_articles_instance = prev_next_articles
 
+prev_next_articles_instance.signal.declare_signal('get_widget', return_type='single')
 
-@Plugin.Signal.connect('article', 'show')
-def article_show(sender, article, left_widgets, **kwargs):
+
+@prev_next_articles_instance.signal.connect_this('get_widget')
+def get_widget(sender, article, **kwargs):
     prev_next_articles = []
     prev_article = Article.query_published().filter(Article.timestamp < article.timestamp).order_by(
-        Article.timestamp.desc()).limit(
-        1).first()
+        Article.timestamp.desc()).limit(1).first()
     if prev_article is not None:
         prev_next_articles.append(prev_article)
     next_article = Article.query_published().filter(Article.timestamp > article.timestamp).order_by(
-        Article.timestamp).limit(
-        1).first()
+        Article.timestamp).limit(1).first()
     if next_article is not None:
         prev_next_articles.append(next_article)
-    left_widgets.append(
-        render_template(prev_next_articles_instance.template_path('widget_content.html'), article=article,
-                        prev_next_articles=prev_next_articles))
+    return {
+        'slug': 'prev_next_articles',
+        'name': '上一篇/下一篇文章',
+        'html': render_template(prev_next_articles_instance.template_path('widget_content.html'),
+                                prev_next_articles=prev_next_articles)
+    }
--- app/plugins/toc/plugin.py
+++ app/plugins/toc/plugin.py
@@ -4,9 +4,15 @@ from ..models import Plugin
 toc = Plugin('文章目录', 'toc')
 toc_instance = toc
 
+toc_instance.signal.declare_signal('get_widget', return_type='single')
 
-@Plugin.Signal.connect('article', 'show.connect')
-def article_show(sender, article, left_widgets, scripts, styles, **kwargs):
-    left_widgets.append(render_template(toc_instance.template_path('widget_content_toc.html'), article=article))
-    scripts.append(render_template(toc_instance.template_path('widget_script_toc.html')))
-    styles.append(render_template(toc_instance.template_path('widget_style_toc.html')))
+
+@toc_instance.signal.connect_this('get_widget')
+def get_widget(sender, article, **kwargs):
+    return {
+        'slug': 'toc',
+        'name': '文章目录',
+        'html': render_template(toc_instance.template_path('widget_content_toc.html'), article=article),
+        'script': render_template(toc_instance.template_path('widget_script_toc.html')),
+        'style': render_template(toc_instance.template_path('widget_style_toc.html'))
+    }
